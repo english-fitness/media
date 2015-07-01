6d5a06636202b1358d501801a332025cf9ab1eaa
thphuong <thp.1093@outlook.com>
2015-07-01 16:26:34 +0700

    bug fixes, update filemanager

--- js/admin/calendar.js
+++ js/admin/calendar.js
@@ -1,31 +1,45 @@
 //A. Calendar pop-up and forms handling
 var CalendarSessionHandler = {
-	newSession: function(values, successCallback, errorCallback, options){
+	newSession: function(values, successCallback, errorCallback){
 		var start = values.start;
 		var day = start.format('YYYY-MM-DD');
-		var startHour = start.format('H');
-		var startMin = start.format('m');
+		var startHour = start.format('HH');
+		var startMin = start.format('mm');
 		
 		var teacher = values.teacher;
+		
+		var classOptions = '';
+		if (classes != undefined){
+			for(var i in classes){
+				classOptions += "<option value=" + classes[i].id + ">" + classes[i].name + "</option>";
+			}
+		}
 		
 		popup({
             width:"450px",
             title:"Tạo buổi học",
             content: function(formCreator){
-				var elementCreator = formCreator.tempForm();				
+				var elementCreator = formCreator.popupForm();				
 				
                 var form = formCreator.getForm({"id":"newSessionForm","method":"post","class":"myFormPopup"});
-                form += formCreator.rowForm("Chủ đề buổi học:",elementCreator.input({"type":"text","name":"Session[subject]"}));
-				form += formCreator.rowForm("Học sinh:", elementCreator.input({"id":"ajaxSearchStudent", "type":"text", "class":"form-control class_email"}));
-				form += formCreator.rowForm("Khóa học:", elementCreator.select({"id":"courseSelect", "name":"Session[course_id]"}, elementCreator.option("", "Chọn khóa học")));
-				form += formCreator.rowForm("Ngày học:", elementCreator.input({"type":"text", "name":"Session[plan_start]", "value":day, "readonly":true}));
-				form += formCreator.rowForm("Thời gian:", 
+                form += formCreator.newRow("Chủ đề buổi học:",elementCreator.input({"type":"text","name":"Session[subject]",id:"sessionTitle"}));
+				form += formCreator.newRow("Học sinh:", elementCreator.input({"id":"ajaxSearchStudent", "type":"text", "class":"form-control class_email"}));
+				form += formCreator.newRow("Khóa học:", elementCreator.select({"id":"courseSelect", "name":"Session[course_id]"}, elementCreator.option("", "Chọn khóa học")));
+				
+				form += '<div class="row-form" id="classSelectRow" style="display:none"><div class="label">Lớp:</div><div class="value">' +
+							elementCreator.select({id:"classSelect"}, classOptions) +
+						'</div></div>';
+				form += '<div class="row-form" id="subjectSelectRow" style="display:none"><div class="label">Môn học:</div><div class="value">' +
+							elementCreator.select({id:"subjectSelect", name:"subjectId", disabled:true}) +
+						'</div></div>';
+				form += formCreator.newRow("Ngày học:", elementCreator.input({"type":"text", "name":"Session[plan_start]", "value":day, "readonly":true}));
+				form += formCreator.newRow("Thời gian:", 
 					elementCreator.input({"type":"text", "name":"startHour", "value":startHour, style:"width:35px; align:center", "readonly":true,}) + 'Giờ&nbsp&nbsp' + 
 					elementCreator.input({"type":"text", "name":"startMin", "value":startMin, style:"width:35px; align:center", "readonly":true}) + ' Phút');
 				form += "<input type='hidden' name='studentId' id='hiddenStudentId'></input>";
 				form += "<input type='hidden' name='Session[plan_duration]' value='30'></input>";
 				form += "<input type='hidden' name='Session[teacher_id]' value="+teacher+"></input>";
-				form += formCreator.rowForm("&nbsp;","<button id='createSession'>Hoàn thành</button>");
+				form += formCreator.newRow("&nbsp;","<button id='createSession'>Hoàn thành</button>");
 				form += '</form>';
                 return form;
             }
@@ -37,47 +51,90 @@ var CalendarSessionHandler = {
 			e.preventDefault();
 			return false;
 		});
+		
+		$('#classSelect').on('change', function(){
+			$.ajax({
+				url:'/admin/course/ajaxLoadSubjectsArray',
+				type:'post',
+				data:{
+					class_id:$(this).val(),
+				},
+				success:function(response){
+					var subjectOptions = '';
+					var subjects = response.subjects;
+					for (var i in subjects){
+						subjectOptions += '<option value=' + subjects[i].id + '>' + subjects[i].name + '</option>';
+					}
+					$('#subjectSelect').html(subjectOptions);
+				}
+			});
+		});
+		
+		$('#courseSelect').on('change', function(){
+			if ($(this).val() > 0){
+				document.getElementById('classSelectRow').style.display = "none";
+				document.getElementById('subjectSelectRow').style.display = "none";
+				document.getElementById('subjectSelect').disabled = true;
+				autoFillSessionTitle($(this).val());
+			} else {
+				document.getElementById('classSelectRow').style.display = "block";
+				$('#classSelect').change();
+				document.getElementById('subjectSelectRow').style.display = "block";
+				document.getElementById('subjectSelect').disabled = false;
+				document.getElementById("sessionTitle").value = "Session 1";
+			}
+		})
 	},
 	
-	editSession : function(data, successCallback, errorCallback, options){
+	editSession : function(data, successCallback, errorCallback){
 		var start = data.start;
 		var day = start.format('YYYY-MM-DD');
-		var startHour = start.format('H');
-		var startMin = start.format('m');
+		var startHour = start.format('HH');
+		var startMin = start.format('mm');
 		
 		popup({
             width:"450px",
             title:"Thông tin buổi học",
             content: function(formCreator){
-				var elementCreator = formCreator.tempForm();			
+				var elementCreator = formCreator.popupForm();			
 				
                 var form = formCreator.getForm({"id":"updateSessionForm","method":"post","class":"myFormPopup"});
-                form += formCreator.rowForm("Chủ đề buổi học:",elementCreator.input({"type":"text","name":"Session[subject]", value:data.subject}));
-				form += formCreator.rowForm("Giáo viên:", data.teacherName);
-				form += formCreator.rowForm("Học sinh:", elementCreator.input({"id":"ajaxSearchStudent","type":"text", "class":"class_email", value:data.student}));
-				form += formCreator.rowForm("Khóa học:", elementCreator.select({"id":"courseSelect", "name":"Session[course_id]"}, elementCreator.option("", data.course_id)));
-				form += formCreator.rowForm("Ngày học:", day);
-				form += formCreator.rowForm("Thời gian:", startHour + '&nbspGiờ&nbsp&nbsp' + startMin + ' Phút');
+                form += formCreator.newRow("Chủ đề buổi học:",elementCreator.input({"type":"text","name":"Session[subject]", value:data.subject}));
+				form += formCreator.newRow("Giáo viên:", data.teacherName);
+				form += formCreator.newRow("Học sinh:", elementCreator.input({"id":"ajaxSearchStudent","type":"text", "class":"class_email", value:data.student}));
+				form += formCreator.newRow("Khóa học:", elementCreator.select({"id":"courseSelect", "name":"Session[course_id]"}, elementCreator.option("", data.course_id)));
+				form += formCreator.newRow("Ngày học:", day);
+				form += formCreator.newRow("Thời gian:", startHour + '&nbspGiờ&nbsp&nbsp' + startMin + ' Phút');
 				form += "<input type='hidden' name='Session[plan_start]' value = '"+ start.format('YYYY-MM-DD HH:mm:ss') +"'></input>";
 				form += "<input type='hidden' name='sessionId' value = '"+ data.sessionId +"'></input>";
 				form += "<input type='hidden' name='studentId' id='hiddenStudentId'></input>";
+				var editRow = "";
+				var deleteRow = "";
 				switch (data.className[0]){
 					case 'ongoingSession':
-						form += formCreator.rowForm("","<a href='#' onclick='endSession("+ data.sessionId +", "+ data.end +");removePopupByID"+'("popupAll")'+";return false;'>Kết thúc buổi học</a>");
+						editRow += "<a href='#' onclick='endSession("+ data.sessionId +", "+ data.end +");removePopupByID"+'("popupAll")'+";return false;'>Kết thúc buổi học</a>&emsp;";
 						break;
 					case 'approvedSession':
-						form += formCreator.rowForm("","<a href='/admin/session/cancel/id/"+ data.sessionId +"' target='_blank'>Hủy buổi học</a>");
+						deleteRow += "<a href='/admin/session/cancel/id/"+ data.sessionId +"' target='_blank'>Hủy buổi học</a>&emsp;";
 						break;
 					case 'pendingSession':
-						form += formCreator.rowForm("","<a href='#' onclick='approveSession("+ data.sessionId +");removePopupByID"+'("popupAll")'+";return false;'>Xác nhận buổi học</a>");
+						form += formCreator.newRow("","<a href='#' onclick='approveSession("+ data.sessionId +");removePopupByID"+'("popupAll")'+";return false;'>Xác nhận buổi học</a>&emsp;");
 						break;
 					default:
 						break;
 				}
 				if (data.className[0] != 'ongoingSession'){
-					form += formCreator.rowForm("","<a href='#' onclick='deleteSession("+ data.sessionId +");removePopupByID"+'("popupAll")'+";return false;' style='color:red'>Xóa buổi học</a>");
+					editRow += "<a href='#' onclick='toggleChangeSchedule(true, "+ data.sessionId +");removePopupByID"+'("popupAll")'+";return false;'>Đổi lịch học</a>&emsp;&nbsp&nbsp";
+					deleteRow += "<a href='#' onclick='deleteSession("+ data.sessionId +");removePopupByID"+'("popupAll")'+";return false;' style='color:red'>Xóa buổi học</a>&emsp;";
 				}
-				form += formCreator.rowForm("&nbsp;","<button id='saveSession'>Lưu lại</button>" +
+				editRow += "<a href='/admin/session/update/id/"+ data.sessionId +"' target='_blank'>Sửa buổi học</a>&emsp;";
+				if (editRow != ""){
+					form += formCreator.newRow("", editRow);
+				}
+				if (deleteRow != ""){
+					form += formCreator.newRow("", deleteRow);
+				}
+				form += formCreator.newRow("&nbsp;","<button id='saveSession'>Lưu lại</button>" +
 													 "<button id='cancel' onclick='removePopupByID"+'("popupAll")'+";return false;'>Đóng</button>");
 				form += '</form>';
                 return form;
