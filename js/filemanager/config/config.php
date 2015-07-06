b3795cd6c6b279b6dc6f840755aa7a6196624a25
thuccoi <thucfami@gmail.com>
2015-07-06 15:29:07 +0700

    upload file toi da 30Mb

--- js/filemanager/config/config.php
+++ js/filemanager/config/config.php
@@ -66,7 +66,7 @@ $access_keys = array('myPrivateKey','someoneElseKey');
 // YOU CAN COPY AND CHANGE THESE VARIABLES INTO FOLDERS config.php FILES TO CUSTOMIZE EACH FOLDER OPTIONS
 //--------------------------------------------------------------------------------------------------------
 
-$MaxSizeUpload = 5; //Mb
+$MaxSizeUpload = 30; //Mb
 
 // SERVER OVERRIDE
 if ((int)(ini_get('post_max_size')) < $MaxSizeUpload){
