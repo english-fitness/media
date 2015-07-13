57462012361e1c6f95de4c824da941ff67d5cce6
thphuong <thp.1093@outlook.com>
2015-07-13 09:17:38 +0700

    add public library

---
+++ applications/filemanager/main/config/publicLibraryConfig.php
@@ -0,0 +1,14 @@
+<?php
+	$delete_files	 = false;
+	$create_folders	 = false;
+	$delete_folders	 = false;
+	$upload_files	 = false;
+	$rename_files	 = false;
+	$rename_folders	 = false;
+	$duplicate_files = false;
+	$copy_cut_files	 = false; // for copy/cut files
+	$copy_cut_dirs	 = false; // for copy/cut directories
+	$edit_text_files = false;
+	$create_text_files = false;
+	$aviary_active		 = false;
+?>
