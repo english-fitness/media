3bfb678f489118d91805b66ffccb69536533ff30
thphuong <thp.1093@outlook.com>
2015-09-29 09:56:18 +0700

    add more loading animation, remove DateUtil since we use moment.js

--- js/utils.js
+++ js/utils.js
@@ -1,23 +1,3 @@
-var DateUtil= {
-    addDay: function(date, amount, delimiter){
-        if (!delimiter){
-            var delimiter = '-';
-        }
-        if (amount == 0){
-            return date.slice(0, 10).replace(/\//g, delimiter);
-        }
-        var denormalizedDate = date.replace(/-/g, '/');
-        var result = new Date(denormalizedDate);
-        result.setDate(result.getDate() + parseInt(amount));
-        
-        var month = (result.getMonth() + 1 < 10) ? '0' + (result.getMonth() + 1) : result.getMonth() + 1;
-        var day = (result.getDate() < 10) ? '0' + (result.getDate()) : result.getDate();
-        
-        //normalize date format
-        return result.getFullYear() + delimiter + month + delimiter + day;
-    }
-}
-
 //searchBoxHandler
 var SearchBox = {
     bindSearchEvent: function(searchBox, searchFunction, searchCallback){
